dc746a39b798e2b9e693041e33a314fc1430394b
GlebSuvakin <iitikbo0216suvakin.g@gmail.com>
2020-09-21 10:52:40 +0300

    Add date detector and change fetch steps for bootstrap

--- js/app.js
+++ js/app.js
@@ -2,10 +2,18 @@ const API_KEY = "oHIyrx47CslUYxiOap4kcqCbtDSIx13c9EjUnfCs";
 const HD_KEY = "true"
 const APOD_URL = `https://api.nasa.gov/planetary/apod?hd=${HD_KEY}&api_key=${API_KEY}`;
 
-const cosmo = document.querySelector('.cosmo');
+const cosmoImg = document.querySelector('.col-md-4');
+const cosmoDescription = document.querySelector('.card-body');
 let title;
 let explanation;
 const promise = fetch(APOD_URL);
+let today = new Date();
+const dd = String(today.getDate()).padStart(2, '0');
+const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+const yyyy = today.getFullYear();
+
+today = dd + '/' + mm + '/' + yyyy;
+console.log(today);
 
 promise.then((responce) => {
   const proccessingPromise = responce.json();
@@ -13,18 +21,23 @@ promise.then((responce) => {
 })
 .then((proccessedResponce) => {
   const h = document.createElement('h2');
-  h.className = 'headline';
+  h.className = 'card-title';
   const img = document.createElement('img');
-  img.className = 'nasa-pic';
+  img.className = 'card-img';
   const p = document.createElement('p');
-  p.className = 'description';
+  p.className = 'card-text';
+  const small = document.createElement('small');
+  small.className = 'text-muted';
   img.src = proccessedResponce.url;
   title = proccessedResponce.title;
   explanation = proccessedResponce.explanation;
+  date = today;
   img.alt = 'Your picture of the day';
-  cosmo.appendChild(h);
-  cosmo.appendChild(img);
-  cosmo.appendChild(p);
+  cosmoImg.appendChild(img);
+  cosmoDescription.appendChild(h);
+  cosmoDescription.appendChild(p);
+  cosmoDescription.appendChild(small);
   document.querySelector('h2').innerHTML = title;
   document.querySelector("p").innerHTML = explanation;
+  document.querySelector('small').innerHTML = date;
 });
